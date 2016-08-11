a5ce79470b261ea878030733bae6b2f057f1cb14
Your Name <arssonist@yahoo.com>
2016-08-11 15:39:58 -0400

    fourth committ excercise 1

--- cats.rb
+++ cats.rb
@@ -7,28 +7,32 @@ class Cats
     @preferred_food = preferred_food
     @meal_time = meal_time
   end
+
+  #part 2
+  # cat1 = Cats.new("Kitty", "Salmon", "Always")
+  # #<Cats:0x007ffa329f8a60 @name="Kitty", @preferred_food="Salmon",
+  # # @meal_time="Always">
+
+# #Q3
+  # #part 1
+  # cat2 = Cats.new("Princess","Turkey", 11)
+  # # #<Cats:0x007ff71a1f09a0 @name="Princess", @preferred_food="Turkey", @meal_time=11>
+  # cat3 = Cats.new("Chicken","Cow", 2)
+  #<Cats:0x007ff71a13fad8 @name="Chicken", @preferred_food="Cow", @meal_time=2>
+#Q4
   def eats_at
     puts "#{@name} likes to eat #{@meal_time}"
   end
 
+# cat1.eats_at
+#"Kitty likes to eat Always"
 
+
+#Q5
   def meow
-    return "My name is #{name} and I eat #{preferred_food} at #{meal_time}"
+    return "My name is #{@name} and I eat #{@preferred_food} at #{@meal_time}"
   end
 end
-#part 2
-# cat1 = Cats.new("Kitty", "Salmon", "Always")
-# #<Cats:0x007ffa329f8a60 @name="Kitty", @preferred_food="Salmon",
-# # @meal_time="Always">
-#
-#
-# #Q3
-# #part 1
-# cat2 = Cats.new("Princess","Turkey", 11)
-# # #<Cats:0x007ff71a1f09a0 @name="Princess", @preferred_food="Turkey", @meal_time=11>
-# cat3 = Cats.new("Chicken","Cow", 2)
-#<Cats:0x007ff71a13fad8 @name="Chicken", @preferred_food="Cow", @meal_time=2>
 
-
-
-#<Cats:0x007fae248293b0 @name="Kitty", @preferred_food="Salmon", @meal_time="Always">
+# cat3.meow
+# "My name is Chicken and I eat Cow at 2"
