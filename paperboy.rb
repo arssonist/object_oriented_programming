83c1d965564445aaf77e6ed746aa0d7dd24e4436
Your Name <arssonist@yahoo.com>
2016-08-11 19:17:34 -0400

    another randome commit on excercise paperboy

--- paperboy.rb
+++ paperboy.rb
@@ -1,15 +1,35 @@
-class Paperboy(name, experience, side, earnings)
+class Paperboy
 
-  def quota
-    experience + 50
+  def initialize(name, side, earnings)
+
+    @name = name
+    @experince = 0
+    @side = side
+    @earnings = earnings
   end
 
-  def deliver(start_address, end_address)
-    (end_address - start_address) + 1
+#   def quota
+#     @experience * 0.5 + 50
+#   end
+#
+  def deliver(end_address, start_address)
+    houses = ((end_address.to_f - start_address + 1) / 2)
+    if @side == "odd"
+      houses.ceil
+    else @side == "even"
+      houses.floor
+    end
   end
-
-  def report
-      puts "I am #{name} and I have made #{earnings}"
-  end
-
 end
+
+
+
+#     @expericence = @experince + houses
+#     puts "Experiece is now #{@experience}"
+#   end
+#
+#   def report
+#       puts "I am #{name} and I have made #{earnings}"
+#   end
+# end
+# # end
