96d485134c28e3e5c744364de83a0c3c0633b1dc
Your Name <arssonist@yahoo.com>
2016-08-12 15:21:25 -0400

    committ oof paperboy working

--- paperboy.rb
+++ paperboy.rb
@@ -1,32 +1,50 @@
 class Paperboy
 
-  def initialize(name, side, earnings)
+  attr_reader :earnings
+  attr_reader :experience
+
+  def initialize(name, side)
 
     @name = name
-    @experince = 0
+    @experience = 0
     @side = side
-    @earnings = earnings
+    @earnings = 0
   end
 
-#   def quota
-#     @experience * 0.5 + 50
-#   end
+  def quota
+    (@experience * 0.5) + 50
+  end
 #
   def deliver(end_address, start_address)
     houses = ((end_address.to_f - start_address + 1) / 2)
     if @side == "odd"
-      houses.ceil
+      houses = houses.ceil
     else @side == "even"
-      houses.floor
+      houses = houses.floor
     end
-  end
+     if houses > quota
+      money_made = (quota * 0.25) + ((houses - quota) * 0.50)
+      else
+       money_made = houses * 0.25
+     end
+      if houses < quota
+       money_made -= 2
+     end
+
+      @earnings += money_made
+      @experience += houses
+
+
+  return money_made
+    end
+
+
+
 end
 
 
 
-#     @expericence = @experince + houses
-#     puts "Experiece is now #{@experience}"
-#   end
+#
 #
 #   def report
 #       puts "I am #{name} and I have made #{earnings}"
