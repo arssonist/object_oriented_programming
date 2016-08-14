0a352deb04c6a40bd8667de0d17476c9554e93b0
Your Name <arssonist@yahoo.com>
2016-08-14 16:41:10 -0400

    second commit of sales tax

--- sales_tax.rb
+++ sales_tax.rb
@@ -1,11 +1,22 @@
 class Item
 
-  def initialize(name, price)
+ attr_reader :name
+ attr_reader :price
+ attr_reader :type
+
+  def initialize(name, price, type)
     @name = name
     @price = price
+    @type = type
+    @tax_rate = 0
+  end
 
+  def tax_calculate(name)
+    if type == book || type == food || type == medcial_products
+      then @tax_rate = 0
+      if type == chocolate_bar || type == music_cd || type == bottle_of_perfume
+        then @ tax_rate = 10%
 
-    
 
 end
 
