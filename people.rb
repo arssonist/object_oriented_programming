a1bb1c52b4fea7073be3032f3722b9abc7a20d88
Your Name <arssonist@yahoo.com>
2016-08-14 15:35:09 -0400

    final commit for persons excersice

--- people.rb
+++ people.rb
@@ -21,6 +21,22 @@ class Instructor < Person
   end
 end
 
+#Q6
 instructor1 = Instructor.new("Chris")
 
 instructor1.greeting
+# Hi, my name is Chris.
+#Q7
+student1 = Student.new("Christina")
+
+student1.greeting
+# Hi, my name is Christina.
+#Q8
+instructor1.teach
+
+# Everything in Ruby is an Object
+student1.learn
+# I get it!
+
+student.teach
+# undefined method- does not work because teach is inside of instructor class, and student does not have access to this area.
