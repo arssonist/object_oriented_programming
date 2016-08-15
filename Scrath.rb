8225f57ea4bdc512b846bce1f52c7b7e83d806da
Your Name <arssonist@yahoo.com>
2016-08-15 16:42:53 -0400

    organization of scratch files

--- Scrath.rb
+++
@@ -1,3 +0,0 @@
-puts "Enter your starting x coordinate, y coordinate and facing direction."
-  x_coordinate, y_coordinate, direction = gets.chomp.split(" ")
-  puts x_coordinate, y_coordinate, direction
--- scratch/Scratch.rb
+++
@@ -1,5 +0,0 @@
-def instructions(input)
-    input.each_char do |instruction|
-end
-end
-puts instructions("nervneovnevn")
